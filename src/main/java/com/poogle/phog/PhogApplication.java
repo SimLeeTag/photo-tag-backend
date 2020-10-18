387b0b7e8acf7b278d62b22706b860c9b85de729
suhyunsim <cnc1106@naver.com>
2020-10-19 01:04:41 +0900

    chore: Initialize project
    - Add dependecies: spring-web, spring-data-jpa, MySQL, Lombok
    
    Issue #3

---
+++ src/main/java/com/poogle/phog/PhogApplication.java
@@ -0,0 +1,13 @@
+package com.poogle.phog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poogle/phog/PhogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poogle.phog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
