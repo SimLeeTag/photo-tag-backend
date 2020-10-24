b2bf02f56e30f9ead0abfd00052242a781636bd4
suhyunsim <cnc1106@naver.com>
2020-10-25 01:10:10 +0900

    feat: Create JwtService
    - `getTokenPayload()`: method decodes String jwt as a map type payloads
    - Payloads contain information of APPLE_KEY and APPLE_ID
    - `checkAppleValidation()`: method checks payloads' validation
    - If the return is false, it will set http status as UNAUTHORIZED.
    - `parseAppleJwt()`: parses the payloads to get the user's information such as name, socialId and email
    
    Issue #9

---
+++ src/main/java/com/poogle/phog/service/JwtService.java
@@ -0,0 +1,57 @@
+package com.poogle.phog.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poogle.phog.domain.User;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    private final String APPLE_KEY;
+    private final String APPLE_ID;
+
+    public JwtService(Environment env) {
+        APPLE_KEY = env.getProperty("APPLE_KEY");
+        APPLE_ID = env.getProperty("APPLE_ID");
+    }
+
+    public Map<String, Object> getTokenPayload(String jwt) {
+        Map<String, Object> payloadMap = new HashMap<>();
+        ObjectMapper om = new ObjectMapper();
+        String encodedTokenPayload = jwt.split("\\.")[1];
+        Base64.Decoder decoder = Base64.getDecoder();
+        String tokenPayload = new String(decoder.decode(encodedTokenPayload));
+        try {
+            payloadMap = om.readValue(tokenPayload, new TypeReference<Map<String, Object>>() {
+            });
+        } catch (Exception e) {
+            System.out.println("[getTokenPayLoad] " + e.getMessage());
+        }
+        return payloadMap;
+    }
+
+    public boolean checkAppleValidation(Map<String, Object> payloads) {
+        //APPLE_KEY
+        String registeredKey = (String) payloads.get("iss");
+        //APPLE_ID
+        String account = (String) payloads.get("aud");
+        return (registeredKey.equals(APPLE_KEY) && account.equals(APPLE_ID));
+    }
+
+    public User parseAppleJwt(Map<String, Object> payloads) {
+        String socialId = (String) payloads.get("socialId");
+        String name = (String) payloads.get("name");
+        String email = (String) payloads.get("email");
+        return User.builder()
+                .name(name)
+                .socialId(socialId)
+                .email(email)
+                .build();
+    }
+}
