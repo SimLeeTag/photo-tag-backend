e5d6ef9c10844e4e44f4d0a0d221778464bcc9ec
suhyunsim <cnc1106@naver.com>
2020-10-29 23:14:22 +0900

    feat: Add TagRepository and refactor Tag entity
    - Add `findTagByTagName()` in `TagRepository`
    - To map the junction table `NoteTag` in bidirection, `Tag` entity contains noteTag lists.

--- src/main/java/com/poogle/phog/domain/Tag.java
+++ src/main/java/com/poogle/phog/domain/Tag.java
@@ -1,19 +1,32 @@
 package com.poogle.phog.domain;
 
-import javax.persistence.*;
+import lombok.*;
 
-@Entity
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity(name = "TAG")
 public class Tag {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "TAG_ID")
-    private Long id;
-
+    @Column(name = "TAG_NAME")
     private String tagName;
 
     private Boolean activated;
 
-    public Tag() {
+    private Long userId;
+
+    @OneToMany(mappedBy = "tag", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<NoteTag> noteTags = new ArrayList<>();
+
+    @Builder
+    public Tag(String tagName, Boolean activated, Long userId) {
+        this.tagName = tagName;
+        this.activated = activated;
+        this.userId = userId;
     }
 }
---
+++ src/main/java/com/poogle/phog/domain/TagRepository.java
@@ -0,0 +1,11 @@
+package com.poogle.phog.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, String> {
+    Optional<Tag> findTagByTagName(String tagName);
+}
