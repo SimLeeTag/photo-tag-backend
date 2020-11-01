64b637547851678c0d56b2f239f78eed15fc66b2
suhyunsim <cnc1106@naver.com>
2020-11-02 05:09:37 +0900

    refactor: Change `@GeneratedValue` strategy `AUTO` to `Identity`
    Issue #15

--- src/main/java/com/poogle/phog/domain/User.java
+++ src/main/java/com/poogle/phog/domain/User.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USER_ID")
     private Long id;
     private String name;
