10a38defcc160d7f3d304a81bef126b1f3a847fc
suhyunsim <cnc1106@naver.com>
2020-11-02 05:30:03 +0900

    refactor: Use query IN clause to optimize requests
    - Before, as many as tags are in the notes, select query should be sent.
    - To optimize the numbers, use IN clause.
    - Add userId to filter tags.
    
    Issue #15

--- src/main/java/com/poogle/phog/domain/TagRepository.java
+++ src/main/java/com/poogle/phog/domain/TagRepository.java
@@ -3,9 +3,10 @@ package com.poogle.phog.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface TagRepository extends JpaRepository<Tag, String> {
-    Optional<Tag> findTagByTagName(String tagName);
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findTagByUserIdAndTagNameIn(Long userId, List<String> tags);
 }
