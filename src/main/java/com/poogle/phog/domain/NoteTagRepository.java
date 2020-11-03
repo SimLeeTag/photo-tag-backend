acf248481364cd2572c2a15c963db91375b1a1b1
suhyunsim <cnc1106@naver.com>
2020-11-03 21:02:30 +0900

    feat: Add `findTagByNoteId()` at NoteTagRepository
    Issue #18

--- src/main/java/com/poogle/phog/domain/NoteTagRepository.java
+++ src/main/java/com/poogle/phog/domain/NoteTagRepository.java
@@ -1,6 +1,13 @@
 package com.poogle.phog.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface NoteTagRepository extends JpaRepository<NoteTag, Long> {
+
+    @Query(value = "SELECT tag FROM NoteTag WHERE note_id = :id")
+    List<Tag> findTagByNoteId(@Param("id") Long noteId);
 }
