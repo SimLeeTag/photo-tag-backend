c23a172bb26b3557a2835cbca3bdf51d7c4f9e9a
suhyunsim <cnc1106@naver.com>
2020-11-04 17:24:42 +0900

    feat: Add `deleteByNoteId()` to delete note_tag table's row
    Issue #16

--- src/main/java/com/poogle/phog/domain/NoteTagRepository.java
+++ src/main/java/com/poogle/phog/domain/NoteTagRepository.java
@@ -1,5 +1,6 @@
 package com.poogle.phog.domain;
 
+import org.hibernate.annotations.SQLDelete;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,4 +11,7 @@ public interface NoteTagRepository extends JpaRepository<NoteTag, Long> {
 
     @Query(value = "SELECT tag FROM NoteTag WHERE note_id = :id")
     List<Tag> findTagByNoteId(@Param("id") Long noteId);
+
+    @SQLDelete(sql = "DELETE FROM note_tag WHERE note_id = :id")
+    void deleteByNoteId(@Param("id") Long noteId);
 }
