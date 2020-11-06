0f587b6bbf1fd35f166107a782cc24df890653f9
suhyunsim <cnc1106@naver.com>
2020-11-06 15:29:34 +0900

    feat: Add `findTagsByUserIdAndActivatedTrueOrderByTagNameAsc()` to TagRepository
    - Find all tags activated and order it ascending by tag name.
    
    Issue #19

--- src/main/java/com/poogle/phog/domain/TagRepository.java
+++ src/main/java/com/poogle/phog/domain/TagRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
     List<Tag> findTagsByUserIdAndTagNameIn(Long userId, List<String> tags);
+    List<Tag> findTagsByUserIdAndActivatedTrueOrderByTagNameAsc(Long userId);
 }
