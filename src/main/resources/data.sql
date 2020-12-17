0f78a6e3c426bbf68318c020f52521e271e79ae4
suhyunsim <cnc1106@naver.com>
2020-12-17 23:44:40 +0900

    style: Delete unused data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,6 @@ INSERT INTO user (email, name, social_id)
 VALUES ("poogle@mail.com", "poogle", 123456);
 INSERT INTO user (email, name, social_id)
 VALUES ("suhyun@mail.com", "suhyun", 654987);
-INSERT INTO user (email, name, social_id)
-VALUES ("lena@lena.com", "lena", 1442341);
 
 INSERT INTO tag (tag_name, activated, user_id)
 VALUES ("ootd", TRUE, 1);
@@ -25,16 +23,6 @@ INSERT INTO tag (tag_name, activated, user_id)
 VALUES ("운동", TRUE, 2);
 INSERT INTO tag (tag_name, activated, user_id)
 VALUES ("ootd", TRUE, 2);
--- INSERT INTO tag (tag_name, activated, user_id)
--- VALUES ("존맛", TRUE, 4);
--- INSERT INTO tag (tag_name, activated, user_id)
--- VALUES ("Flex", TRUE, 4);
--- INSERT INTO tag (tag_name, activated, user_id)
--- VALUES ("운동", TRUE, 4);
--- INSERT INTO tag (tag_name, activated, user_id)
--- VALUES ("ootd", TRUE, 4);
--- INSERT INTO tag (tag_name, activated, user_id)
--- VALUES ("shopping", TRUE, 4);
 
 INSERT INTO note (created, is_deleted, raw_memo, user_id)
 VALUES ("2020-06-19 13:34:11", FALSE, "Today I had a great lunch. #Flex #존맛", 1);
@@ -46,14 +34,7 @@ INSERT INTO note (created, is_deleted, raw_memo, user_id)
 VALUES ("2020-10-20 11:20:30", FALSE, "#ootd #shopping #Flex", 2);
 INSERT INTO note (created, is_deleted, raw_memo, user_id)
 VALUES ("2020-10-31 08:34:10", FALSE, "#운동 열심히 합시다. #ootd", 2);
--- INSERT INTO note (created, is_deleted, raw_memo, user_id)
--- VALUES ("2020-07-22 16:21:34", FALSE, "Happy Birthday #Flex #존맛", 4);
--- INSERT INTO note (created, is_deleted, raw_memo, user_id)
--- VALUES ("2020-10-20 11:20:30", FALSE, "#ootd #shopping #Flex", 4);
--- INSERT INTO note (created, is_deleted, raw_memo, user_id)
--- VALUES ("2020-10-31 08:34:10", FALSE, "#운동 열심히 합시다. #ootd", 4);
 
---
 INSERT INTO note_tag (note_id, tag_id) VALUES (1, 4);
 INSERT INTO note_tag (note_id, tag_id) VALUES (1, 6);
 INSERT INTO note_tag (note_id, tag_id) VALUES (2, 3);
@@ -66,11 +47,3 @@ INSERT INTO note_tag (note_id, tag_id) VALUES (4, 2);
 INSERT INTO note_tag (note_id, tag_id) VALUES (4, 8);
 INSERT INTO note_tag (note_id, tag_id) VALUES (5, 9);
 INSERT INTO note_tag (note_id, tag_id) VALUES (5, 10);
--- INSERT INTO note_tag (note_id, tag_id) VALUES (6, 11);
--- INSERT INTO note_tag (note_id, tag_id) VALUES (6, 12);
--- INSERT INTO note_tag (note_id, tag_id) VALUES (7, 12);
--- INSERT INTO note_tag (note_id, tag_id) VALUES (7, 14);
--- INSERT INTO note_tag (note_id, tag_id) VALUES (7, 15);
--- INSERT INTO note_tag (note_id, tag_id) VALUES (8, 14);
--- INSERT INTO note_tag (note_id, tag_id) VALUES (8, 14);
-
