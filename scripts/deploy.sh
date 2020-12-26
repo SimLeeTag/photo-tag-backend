14983cbecd74b81c6b586b62425955d3b0cc32d4
suhyunsim <cnc1106@naver.com>
2020-12-27 01:28:07 +0900

    chore: Add `deploy.sh` to application
    - After build the application `deploy.sh` will be executed.
    - Delete useless `execute-deploy.sh` because it was integrated in
    `deploy.sh` file.
    
    Issue #25

---
+++ scripts/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/photo-tag-backend
+PROJECT_NAME=phog
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/jar/
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/jar/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> JAR_NAME에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar $REPOSITORY/jar/$JAR_NAME --spring.profiles.active=deploy 2>&1 &
