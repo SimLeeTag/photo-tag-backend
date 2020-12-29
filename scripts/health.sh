e2d7a583a59d22cfc7a7009e59ac50b204016b18
suhyunsim <cnc1106@naver.com>
2020-12-29 23:30:48 +0900

    fix: Fix typo

--- scripts/health.sh
+++ scripts/health.sh
@@ -19,12 +19,12 @@ do
 
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 (deploy 문자열 있는지 검증)
-    echo "> Health check 성공"
-    switch_proxy
-    break
+      echo "> Health check 성공"
+      switch_proxy
+      break
   else
-    echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-    echo "> Health check: ${RESPONSE}"
+      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+      echo "> Health check: ${RESPONSE}"
   fi
 
   if [ ${RETRY_COUNT} -eq 10 ]
