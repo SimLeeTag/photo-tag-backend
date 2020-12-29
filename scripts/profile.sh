bd1cb9dc733700cf702e3ba2625edd934fb6b508
suhyunsim <cnc1106@naver.com>
2020-12-30 00:29:55 +0900

    fix: Check Get Mapping `profile` at the non-switching situtation

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -4,13 +4,13 @@
 
 # 쉬고 있는 profile 찾기: deploy1이 사용중이면 deploy2가 쉬고 있고, 반대면 deploy1이 쉬고 있음
 function find_idle_profile() {
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/profile)
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
         CURRENT_PROFILE=deploy2
     else
-        CURRENT_PROFILE=$(curl -s http://localhost:8080/profile)
+        CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
 
     if [ ${CURRENT_PROFILE} == deploy1 ]
