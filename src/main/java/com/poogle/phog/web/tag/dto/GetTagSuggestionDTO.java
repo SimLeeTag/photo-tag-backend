2fe7e0ae5a11e12f8fb4f478c7e172e87b04a1c5
suhyunsim <cnc1106@naver.com>
2021-01-05 03:12:30 +0900

    refactor: Specify attributes of GetTagSuggestion
    - Specify tags into two types to suggest both Korean and English tags.
    
    Issue #33

--- src/main/java/com/poogle/phog/web/tag/dto/GetTagSuggestionDTO.java
+++ src/main/java/com/poogle/phog/web/tag/dto/GetTagSuggestionDTO.java
@@ -1,15 +1,22 @@
 package com.poogle.phog.web.tag.dto;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
 @ToString
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class GetTagSuggestionDTO {
-    private List<String> suggestion;
+    private List<String> tagsEn;
+    private List<String> tagsKr;
 
+    @Builder
+    public GetTagSuggestionDTO(List<String> tagsEn, List<String> tagsKr) {
+        this.tagsEn = tagsEn;
+        this.tagsKr = tagsKr;
+    }
 }
