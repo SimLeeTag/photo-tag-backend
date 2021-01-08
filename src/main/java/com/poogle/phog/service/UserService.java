af051ed1d884e92b559acf6020b7825f8d79062e
suhyunsim <cnc1106@naver.com>
2021-01-08 22:56:43 +0900

    feat: Add `findUserList()` to UserService
    - Find all users in user table.
    
    Issue #49

--- src/main/java/com/poogle/phog/service/UserService.java
+++ src/main/java/com/poogle/phog/service/UserService.java
@@ -2,10 +2,14 @@ package com.poogle.phog.service;
 
 import com.poogle.phog.domain.User;
 import com.poogle.phog.domain.UserRepository;
+import com.poogle.phog.web.admin.user.dto.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class UserService {
 
@@ -24,5 +28,11 @@ public class UserService {
         } else {
             userRepository.save(user);
         }
+    }
+
+    public ResponseDTO findUserList() {
+        List<User> users = userRepository.findAll();
+        log.debug("[*] users: {}", users);
+        return new ResponseDTO(200, "전체 유저 목록", users);
     }
 }
